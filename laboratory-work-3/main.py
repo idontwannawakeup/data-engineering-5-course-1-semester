bb44306bf96a1df99648c7049cfd8e8a0e6559db
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2023-12-17 02:47:22 +0200

    [features] Laboratory Work 3 - Refactoring and optimization

--- laboratory-work-3/main.py
+++ laboratory-work-3/main.py
@@ -1,7 +1,6 @@
 import csv
 import json
 import os
-import boto3
 
 
 def flatten_json(nested_json):
@@ -28,32 +27,20 @@ def get_json_file_content(full_filename: str):
         return json.load(json_file)
 
 
-def get_json_files_from_folders(folder_path: str):
-    json_files = []
-    for root, _, files in os.walk(folder_path):
-        for file in files:
-            full_filename = os.path.join(root, file) 
-            if full_filename.endswith(".json"):
-                json_files.append({
-                    "filename": full_filename,
-                    "content": flatten_json(get_json_file_content(full_filename))
-                })
-
-    return json_files
-
-
-def write_json_to_csv(json_files):
-    for json_file in json_files:
-        json_keys = list(json_file["content"].keys())
-        with open(json_file["filename"].replace(".json", ".csv"), 'w') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow(json_keys)
-            writer.writerow([json_file["content"][json_key] for json_key in json_keys])
+def write_json_to_csv(json_file):
+    json_keys = list(json_file["content"].keys())
+    with open(json_file["filename"].replace(".json", ".csv"), 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(json_keys)
+        writer.writerow([json_file["content"][json_key] for json_key in json_keys])
 
 
 def main():
-    json_files = get_json_files_from_folders("./data")
-    write_json_to_csv(json_files)
+    for root, _, files in os.walk("./data"):
+        for file in files:
+            full_filename = os.path.join(root, file) 
+            if full_filename.endswith(".json"):
+                write_json_to_csv({"filename": full_filename, "content": flatten_json(get_json_file_content(full_filename))})
 
 
 if __name__ == "__main__":
