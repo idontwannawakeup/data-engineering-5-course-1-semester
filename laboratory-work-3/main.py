4c278ff38c04dc185a7817c8e43cf87c483ba0e3
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2023-12-17 02:19:02 +0200

    [features] Laboratory Work 3 - Json flatten implementation

--- laboratory-work-3/main.py
+++ laboratory-work-3/main.py
@@ -3,6 +3,25 @@ import os
 import boto3
 
 
+def flatten_json(nested_json):
+   flattened_json = {}
+
+   def flatten(x, name=''):
+      if type(x) is dict:
+         for a in x:
+            flatten(x[a], name + a + '_')
+      elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+      else:
+         flattened_json[name[:-1]] = x
+
+   flatten(nested_json)
+   return flattened_json
+
+
 def get_json_file_content(full_filename: str):
     with open(full_filename) as json_file:
         return json.load(json_file)
@@ -16,7 +35,7 @@ def get_json_files_from_folders(folder_path: str):
             if full_filename.endswith(".json"):
                 json_files.append({
                     "filename": full_filename,
-                    "content": get_json_file_content(full_filename)
+                    "content": flatten_json(get_json_file_content(full_filename))
                 })
 
     return json_files
@@ -25,6 +44,7 @@ def get_json_files_from_folders(folder_path: str):
 def main():
     json_files = get_json_files_from_folders("./data")
     print(json_files)
+
 
 if __name__ == "__main__":
     main()
