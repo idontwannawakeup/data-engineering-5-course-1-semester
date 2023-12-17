a4a8f9b72320c0b45988695f184b60706daa37a6
Vasyl Semeniuk <semeniuk.vasyl.r@gmail.com>
2023-12-17 02:12:29 +0200

    [features] Laboratory Work 3 - Json files read

--- laboratory-work-3/main.py
+++ laboratory-work-3/main.py
@@ -1,10 +1,30 @@
+import json
+import os
 import boto3
 
 
-def main():
-    # your code here
-    pass
+def get_json_file_content(full_filename: str):
+    with open(full_filename) as json_file:
+        return json.load(json_file)
 
+
+def get_json_files_from_folders(folder_path: str):
+    json_files = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            full_filename = os.path.join(root, file) 
+            if full_filename.endswith(".json"):
+                json_files.append({
+                    "filename": full_filename,
+                    "content": get_json_file_content(full_filename)
+                })
+
+    return json_files
+
+
+def main():
+    json_files = get_json_files_from_folders("./data")
+    print(json_files)
 
 if __name__ == "__main__":
     main()
